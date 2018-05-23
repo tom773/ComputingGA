ecd55215e406e23d60857c59d3110ccd3c033764
Tom Matthews <tom.matthews68@yahoo.com.au>
2018-05-23 10:04:49 +1000

    Update mtgox.lisp

--- pages/mtgox.lisp
+++ pages/mtgox.lisp
@@ -18,5 +18,11 @@
 	      "They were able to restore the coins and trading was back to normal soon after. "
 	      (:h2 "Bitcoin theft")
 	      "From 7 Feburary 2014, Mt Gox started showing issues. Withdrawls were halted, after the owners discovered transactions could be modified (\"malleability\") while still appearing valid to Bitcoin nodes. "
+	      (:h2 "A Bitcoin Suicide")
+	      "On The 4th of December, 2017 a disturbing post hit the r/bitcoin subreddit detailing the hard struggles of man who lost millions in Bitcoin the Mt Gox hack."
+	      (:br)
+	      "At the time the Bitcoin was not worth much however with the metoric rise of the cryptocurrency in late 2017 came individuals who had spent many Bitcoins in the past kicking themselves."
+	      (:br)
+	      "Unfortunatley this proved to much for one individual who's lost bitcoin amounted to almost $159,000,000, and he had no way of getting it back. He sadly took his own life late November, 2017. R.I.P"
 	      (:hr)
 	      (footnote-collector))))
